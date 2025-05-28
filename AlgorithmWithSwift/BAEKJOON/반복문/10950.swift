7f658cd1c6892dd76a47c20c49bb22e20a07c1ee
junil1030 <dccrdseo@naver.com>
2025-05-28 17:29:58 +0900

    3단계 반복문 (2739, 10950)

---
+++ AlgorithmWithSwift/BAEKJOON/반복문/10950.swift
@@ -0,0 +1,26 @@
+//
+//  10950.swift
+//  AlgorithmWithSwift
+//
+//  Created by 서준일 on 5/20/25.
+//
+
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+// 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// 출력
+// 각 테스트 케이스마다 A+B를 출력한다.
+
+func q10950() {
+    let tc = Int(readLine()!)!
+    
+    for _ in 0..<tc {
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
+        
+        print(input[0] + input[1])
+    }
+}
