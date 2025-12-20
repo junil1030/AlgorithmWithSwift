32ac41c1f698b2a83e29069b51f67a300d58e88b
junil1030 <dccrdseo@naver.com>
2025-12-20 15:13:09 +0900

    feat: 2178, 1012 문제 풀이

--- AlgorithmWithSwift/BAEKJOON/Graph/2178.swift
+++ AlgorithmWithSwift/BAEKJOON/Graph/2178.swift
@@ -8,42 +8,42 @@
 import Foundation
 
 func q2178() {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-    let n = input[0]
-    let m = input[1]
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    let y = input[0]; let x = input[1];
     
-    var board: [[Int]] = .init(repeating: [], count: n)
-    for i in 0..<n {
-        let input = readLine()!.split(separator: "").map { Int($0)! }
-        board[i].append(contentsOf: input)
+    var board: [[Int]] = []
+    var visited: [[Int]] = .init(repeating: .init(repeating: 0, count: x), count: y)
+    
+    for _ in 0..<y {
+        let input = readLine()!.map { Int(String($0))! }
+        board.append(input)
     }
     
-    var visited: [[Int]] = .init(repeating: .init(repeating: -1, count: m), count: n)
-    
-    let dy = [0, 1, 0, -1]
-    let dx = [1, 0, -1 ,0]
+    let dy = [1, -1, 0, 0]
+    let dx = [0, 0, -1, 1]
     
     var queue: [(Int, Int)] = []
     var front = 0
+    let start = (0, 0)
     
-    queue.append((0,0))
     visited[0][0] = 1
+    queue.append(start)
     
     while queue.count >= front + 1 {
         let cur = queue[front]
         front += 1
         
-        for dir in 0..<4 {
-            let ny = cur.0 + dy[dir], nx = cur.1 + dx[dir]
+        for i in 0..<4 {
+            let ny = cur.0 + dy[i]
+            let nx = cur.1 + dx[i]
             
-            if ny < 0 || ny >= n || nx < 0 || nx >= m { continue }
-            if visited[ny][nx] != -1 { continue }
+            if ny < 0 || ny >= y || nx < 0 || nx >= x { continue }
             if board[ny][nx] == 0 { continue }
-            
-            queue.append((ny, nx))
+            if visited[ny][nx] > 0 { continue }
             visited[ny][nx] = visited[cur.0][cur.1] + 1
+            queue.append((ny, nx))
         }
     }
     
-    print(visited[n - 1][m - 1])
+    print(visited[y - 1][x - 1])
 }
